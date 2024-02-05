bd75d98910e12e50a5e314672060f6f5d5d212ab
Sammed Sagare <76863715+sammedsagare@users.noreply.github.com>
2024-02-05 12:27:17 +0530

    intro to ts and basic types

---
+++ Typescript/01/src/main.ts
@@ -0,0 +1,10 @@
+let username = "Sammed"
+console.log(username);
+
+let a = 12
+let b = '6'
+let c = 2
+
+console.log(a / b);
+console.log(c*b);
+
---
+++ Typescript/02/src/main.ts
@@ -0,0 +1,18 @@
+let myName: string = "Sammed"
+let meaningOfLife : number
+let isLoading: boolean
+let album: any
+
+myName = "Sammed Sagare"
+meaningOfLife = 18
+isLoading = true
+album = 1234567890
+
+const sum = (a: number, b: string) => {
+    return a+b
+}
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp = /\w+/g
