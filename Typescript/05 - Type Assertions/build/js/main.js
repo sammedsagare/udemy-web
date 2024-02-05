60ab0ebf9af6097b8d2f262140d17504c3bbd38a
Sammed Sagare <76863715+sammedsagare@users.noreply.github.com>
2024-02-05 18:50:49 +0530

    typescript assertions

---
+++ Typescript/05 - Type Assertions/build/js/main.js
@@ -0,0 +1,20 @@
+"use strict";
+// convert to more or less specific
+let a = "hello";
+let b = a; // less specific
+let c = a;
+let d = 'world';
+let e = 'world';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
+};
+let myVal = addOrConcat(2, 2, 'concat');
+// Be careful. TS sees no problem - but a string is returned.
+let nextVal = addOrConcat(2, 2, 'concat');
+const img = document.querySelector('img');
+const myImg = document.getElementById('#img');
+const nextImg = document.getElementById('#img');
+img.src;
+myImg.src;
